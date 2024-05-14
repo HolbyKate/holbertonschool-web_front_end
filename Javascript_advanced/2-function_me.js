8008fcdf21380719d0188b80984c00c30b0e71bd
HolbyKate <6804@holbertonstudents.com>
2024-05-14 08:41:12 +0000

    Create a welcome message

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,11 @@
+// Closure with name //
+
+function welcomeMessage(fullName) {
+    return function() {
+        alert("Welcome" + " " + fullName);
+    }
+}
+
+let guillaume  = welcomeMessage("Guillaume");
+let alex = welcomeMessage("Alex");
+let fred = welcomeMessage("Fred");
