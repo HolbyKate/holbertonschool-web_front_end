679a8b08daa26714674e64a0e16b1fedb8573f18
HolbyKate <6804@holbertonstudents.com>
2024-05-15 15:23:07 +0000

    Define area of object

---
+++ Javascript_advanced/12-room_area.js
@@ -0,0 +1,12 @@
+// Create an object named roomDimensions //
+
+let roomDimensions = {
+    width: 50,
+    length: 100,
+    getArea () {
+        return this.width * this.length;
+    }
+}
+
+const boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+
