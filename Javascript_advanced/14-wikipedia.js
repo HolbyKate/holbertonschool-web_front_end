1dfae5209ce3eb3103cb5788eca1d899081244d3
HolbyKate <6804@holbertonstudents.com>
2024-05-22 09:12:57 +0000

    Fix code thank Truuue

--- Javascript_advanced/14-wikipedia.js
+++ Javascript_advanced/14-wikipedia.js
@@ -7,20 +7,22 @@ function createElement(data) {
 }
 
 function queryWikipedia(callback) {
-    const xhttp = new XMLHttpRequest();
-    const url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*";
+  const xml = new XMLHttpRequest();
+  const url =
+    "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*";
 
-    xhttp.open('GET, url, true');
-
-    xhttp.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            const pages = response.query.pages;
-            const pageId = Object.keys(pages)[0];
-            const extract = pages[pageId].extract;
-            callback(extract);
-        }
-    };
-
-    xhttp.send();
+  xml.open("GET", url, true);
+  xml.onreadystatechange = function () {
+    if (xml.readyState === 4 && xml.status === 200) {
+      const response = JSON.parse(xml.responseText);
+      const extract =
+        response.query.pages[Object.keys(response.query.pages)[0]].extract;
+      callback(extract);
+    }
+  };
+  xml.send();
 }
+
+queryWikipedia(function (extract) {
+  createElement(extract);
+});
